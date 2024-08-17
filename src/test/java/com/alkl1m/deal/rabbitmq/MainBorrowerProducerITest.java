230b45dc430211f9b7e1d9d3f72a48f6f8a61d9f
AlKl1M <al.klimkovetskiy@gmail.com>
2024-08-17 18:06:26 +0200

    test: добавил тесты на очередь недоставленных сообщений

--- src/test/java/com/alkl1m/deal/rabbitmq/MainBorrowerProducerITest.java
+++ src/test/java/com/alkl1m/deal/rabbitmq/MainBorrowerProducerITest.java
@@ -2,18 +2,13 @@ package com.alkl1m.deal.rabbitmq;
 
 import com.alkl1m.deal.config.MQConfiguration;
 import com.alkl1m.deal.web.payload.MainBorrowerRequest;
-import com.alkl1m.deal.web.payload.UpdateContractorMessage;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.rabbit.listener.RabbitListenerEndpointRegistry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.system.OutputCaptureExtension;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
-import org.springframework.transaction.annotation.Transactional;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.containers.RabbitMQContainer;
 import org.testcontainers.junit.jupiter.Container;
@@ -63,8 +58,6 @@ class MainBorrowerProducerITest {
         Thread.sleep(5000);
 
         MainBorrowerRequest receivedMessage = (MainBorrowerRequest) rabbitTemplate.receiveAndConvert(MQConfiguration.UPDATE_MAIN_BORROWER_QUEUE);
-
-        System.out.println(receivedMessage);
 
         assertThat(receivedMessage).isNotNull();
         assertThat(receivedMessage.contractorId()).isEqualTo("1");
