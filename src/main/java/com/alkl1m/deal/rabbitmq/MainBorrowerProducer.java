50be4ce9300632ed5de9ad0117d378564df795c7
AlKl1M <al.klimkovetskiy@gmail.com>
2024-08-18 15:30:22 +0200

    docs: добавил javadoc

--- src/main/java/com/alkl1m/deal/rabbitmq/MainBorrowerProducer.java
+++ src/main/java/com/alkl1m/deal/rabbitmq/MainBorrowerProducer.java
@@ -6,13 +6,25 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Component;
 
+/**
+ * Класс-производитель для отправки сообщений о главном заемщике.
+ *
+ * @author alkl1m
+ */
 @Component
 @RequiredArgsConstructor
 public class MainBorrowerProducer {
+
     private final RabbitTemplate rabbitTemplate;
 
+    /**
+     * Отправляет сообщение о главном заемщике в очередь RabbitMQ.
+     *
+     * @param msg сообщение, содержащее данные о главном заемщике.
+     */
     public void sendMessage(MainBorrowerRequest msg) {
-        rabbitTemplate.convertAndSend(MQConfiguration.DEALS_UPDATE_MAIN_BORROWER_EXC, MQConfiguration.CONTRACTOR_UPDATE_MAIN_BORROWER_QUEUE, msg);
+        rabbitTemplate.convertAndSend(MQConfiguration.DEALS_UPDATE_MAIN_BORROWER_EXC,
+                MQConfiguration.CONTRACTOR_UPDATE_MAIN_BORROWER_QUEUE,
+                msg);
     }
-
 }
